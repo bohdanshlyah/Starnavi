4db13553d7b322f5d2f8fdf3660710ba128a4ee3
bohdanshlyah <57491073+bohdanshlyah@users.noreply.github.com>
2021-04-28 10:45:27 +0300

    add urls

--- SocialNetwork/SocialNetwork/urls.py
+++ SocialNetwork/SocialNetwork/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('User.urls')),
+    path('api/post/', include('Post.urls')),
 
 ]
